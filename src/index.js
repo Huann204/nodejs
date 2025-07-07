bc4ef6766c1513eb609293742cd384556b5c923d
Your NameHuân <nguyenhuan26012004@gmail.com>
2025-07-07 22:31:12 +0700

    Template engine (handlebars)

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars'); // 👈 Đúng cách
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', engine({
+    extname: '.hbs',
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'resources', 'views', 'layouts'),
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views')); // 👈 Cẩn thận dấu /
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+app.get('/news', (req, res) => {
+    res.render('news');
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
