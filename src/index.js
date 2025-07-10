ba950ae1ec4719729bc27a394459ad3ca27c9120
Your NameHuân <nguyenhuan26012004@gmail.com>
2025-07-10 23:43:31 +0700

    [MVC] Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
-const { log } = require('console');
+
 
 const app = express();
 const port = 3000;
+
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded(
@@ -26,21 +28,10 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
+//Routes init
+route(app)
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-app.get('/news', (req, res) => {
-    res.render('news');
-});
-app.get('/search', (req, res) => {
-    res.render('search');
-});
-app.post('/search', (req, res) => {
-    console.log(req.body);
 
-    res.send('');
-});
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
